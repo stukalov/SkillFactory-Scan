09e88879cd429f05b7da66c0d657f4f53d270fac
Aleksei <franz@kustik.ru>
2023-08-22 19:07:49 +0300

    )

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+      </header>
+      <main className="main">
+          <section className="section section-1">
+              <p className="text-1">сервис по поиску<br/>публикаций<br/>о компании<br/>по его ИНН</p>
+              <p className="text-2">Комплексный анализ публикаций, получение данных<br/>в формате PDF на электронную почту.</p>
+              <button className="button-1">Запросить данные</button>
+              <img className="image-1" src="static/main-image.jpg" alt="" />
+          </section>
+          <section className="section section-2">
+2
+          </section>
+          <section className="section section-3">
+3
+          </section>
+          <section className="section section-4">
+4
+          </section>
+      </main>
+      <footer className="footer">
+      </footer>
+    </div>
+  );
+}
+
+export default App;
